e21a96a2d3ec06278ffafc112050973fec1b202f
ethan.conrad <ethan.conrad@netvalue.nz>
2026-03-26 09:26:38 +1300

    Update base path to match new repo name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import reactRefresh from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: '/redlid-website/',
   build: {
     outDir: 'build',
   },
