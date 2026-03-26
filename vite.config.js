b75d777c3534c61334e08d306933ad8fd5c1878a
ethan.conrad <ethan.conrad@netvalue.nz>
2026-03-27 08:17:17 +1300

    Change base URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import reactRefresh from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: '/redlid-website/',
+  base: '/',
 })
